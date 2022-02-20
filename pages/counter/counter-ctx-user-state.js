d595b9217f08e0e560c6af2b57bd849018c61e26
Jack Park <jack.park@gmail.com>
2022-02-20 22:46:51 +0900

    Step1 - useContext

--- pages/counter/counter-ctx-user-state.js
+++ pages/counter/counter-ctx-user-state.js
@@ -28,11 +28,22 @@ const Counter = () => {
   return <div>Counter: {counter}</div>;
 };
 
-export default function CounterUseState() {
+function CounterUseState() {
   return (
     <CounterContextProvider>
       <Container />
       <Counter />
     </CounterContextProvider>
+  );
+}
+
+export default function CounterUserContextPage() {
+  return (
+    <div>
+      <CounterUseState />
+      <CounterUseState />
+      <CounterUseState />
+      <CounterUseState />
+    </div>
   );
 }
