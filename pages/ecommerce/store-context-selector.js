f06430f764b6a5fcdba56f0277e928a2507cc8e8
Jack Park <jack.park@gmail.com>
2022-02-21 00:30:06 +0900

    use-context-selector default

---
+++ pages/ecommerce/store-context-selector.js
@@ -0,0 +1,29 @@
+import React, { useState, createContext, useContext } from "react";
+
+const useStore = () => {
+  const [user, setUser] = useState("");
+  const [cartCount, setCartCount] = useState(0);
+
+  return {
+    user,
+    cartCount,
+    login: () => setUser("Jack"),
+    logout: () => setUser(""),
+    addToCart: () => setCartCount(cartCount + 1),
+  };
+};
+
+const StoreContext = createContext(null);
+
+export const StoreContextProvider = ({ children }) => {
+  return (
+    <StoreContext.Provider value={useStore()}>{children}</StoreContext.Provider>
+  );
+};
+
+export const useLogin = () => useContext(StoreContext).login;
+export const useLogout = () => useContext(StoreContext).logout;
+export const useAddToCart = () => useContext(StoreContext).addToCart;
+
+export const useUser = () => useContext(StoreContext).user;
+export const useCartCount = () => useContext(StoreContext).cartCount;
